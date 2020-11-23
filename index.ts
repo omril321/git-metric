e41ddab8d33c52020600ed92fa06f00692fe9655
Omri Lavi <omri@testim.io>
2020-11-23 22:13:57 +0200

    spreading the repo history

--- index.ts
+++ index.ts
@@ -2,9 +2,12 @@ import os from 'os';
 import fse from 'fs-extra';
 import path from 'path';
 import gitlog from "gitlog";
+import * as child_process from 'child_process';
 
+const REPO_NAME = 'repo_with_history';
 const analyzedRepoPath = '/Users/omri/private/repos/repo_with_history/';
-const copiedProjPath = path.resolve(os.tmpdir(), 'repo_with_history/');
+const copiedProjPath = path.resolve(os.tmpdir(), REPO_NAME, 'root');
+const CLEAN_BEFORE_CLONE = true;
 
 
 async function copyProjectToTempDir() {
@@ -13,9 +16,9 @@ async function copyProjectToTempDir() {
     console.log(`successfully copied from ${analyzedRepoPath} to ${copiedProjPath}`);
 }
 
-async function getCommitHashes() {
+function getCommitHashes() {
     return gitlog({
-        repo: analyzedRepoPath,
+        repo: copiedProjPath,
         before: '22-10-2020',
         until:  '01-11-2021', //TODO: dynamic range
         number: 50, //TODO: dynamic limit
@@ -23,10 +26,26 @@ async function getCommitHashes() {
     });
 }
 
+function cloneCommitsToFolders(commits: ReturnType<typeof getCommitHashes>) {
+    const withCloneDetails = commits.map(commit => ({ ...commit, cloneDestination: path.resolve(os.tmpdir(), REPO_NAME, commit.hash) }));
+    withCloneDetails.forEach((commitData) => {
+        if(CLEAN_BEFORE_CLONE) {
+            fse.removeSync(commitData.cloneDestination);
+        }
+        child_process.execSync(`git checkout ${commitData.hash}`, { cwd: copiedProjPath });
+        child_process.execSync(`git checkout-index -a --prefix=${commitData.cloneDestination}/`, { cwd: copiedProjPath });
+    });
+
+    return withCloneDetails;
+}
+
 async function run() {
     await copyProjectToTempDir();
     const commitHashes = getCommitHashes();
-    console.log(commitHashes);
+    const commitsWithCloneDetails = await cloneCommitsToFolders(commitHashes);
+
+
+    console.log(commitsWithCloneDetails);
 }
 
 run();

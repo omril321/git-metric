a363927227280bb34682e83431c1a7972b08eb2f
Omri Lavi <omri@testim.io>
2020-12-26 17:40:49 +0200

    added some tests

--- test/e2e/e2e.test.ts
+++ test/e2e/e2e.test.ts
@@ -1,5 +1,31 @@
+import { run } from '../../src';
+import { GitRepoForTests } from './utils';
+
 describe('e2e', () => {
-    it('should pass', () => {
-        expect('yo').toEqual('yo');
-    });
+
+    (['differential', 'full-snapshot'] as const).forEach(strategy => {
+        it(`should count files metrics properly for a repo with a single commit using strategy ${strategy}`, async () => {
+            const repo = new GitRepoForTests()
+            await repo.init();
+
+            await repo.executeCommits(
+                [
+                    { create: ['file1.ts'] }
+                ]
+            )
+            const result = await run({
+                metricNameToGlob: { tsFilesForThisTest: ['**.ts'], txtFiles: ['**.txt'] },
+                repositoryPath: repo.path!,
+                strategy,
+                task: 'count-files',
+                ignoreModifiedFiles: true,
+                maxCommitsCount: 10,
+                commitsSince: new Date(new Date().setDate(new Date().getDate() - 1)).toISOString(), //yesterday
+            })
+
+            expect(result).toHaveLength(1);
+            expect(result[0].metrics).toEqual({ tsFilesForThisTest: 1, txtFiles: 0 });
+        });
+    })
+
 });
---
+++ test/e2e/utils.ts
@@ -0,0 +1,89 @@
+import os from 'os';
+import path from 'path';
+import git from 'isomorphic-git';
+import fse from 'fs-extra';
+import _ from 'lodash';
+
+
+const TMP_REPOS_PATH = path.resolve(os.tmpdir(), 'project-metrics-e2e');
+
+type CommitActions = {
+    create: string[];
+    remove: string[];
+    modifyContent: string[];
+    rename: { from: string, to: string, modifyContent?: boolean }[];
+    message: string;
+}
+
+export class GitRepoForTests {
+    public path?: string;
+
+    constructor() {
+    }
+
+    public async init() {
+        this.path = path.resolve(TMP_REPOS_PATH, `${Date.now()}`);
+        await git.init({fs: fse, dir: this.path});
+        return this.path;
+    }
+
+    public async executeCommits(fileOperations: Partial<CommitActions>[]) {
+        await Promise.all(fileOperations.map((ops) => this.executeCommit(ops)));
+    }
+
+    private async executeCommit({ create = [], remove = [], rename = [], modifyContent = [], message = `Commit ${Date.now()}` }: Partial<CommitActions>) {
+        if (!this.path) {
+            throw new Error('Git repo is not initiated')
+        }
+
+        await Promise.all(create.map((relativePath) => this.createNewFile(relativePath)));
+        await Promise.all(remove.map((relativePath) => this.deleteExistingFile(relativePath)));
+        await Promise.all(rename.filter(({ modifyContent }) => modifyContent).map(({ from }) => this.modifyExistingFile(from)));
+        await Promise.all(rename.map(({ from, to }) => this.renameExistingFile(from, to)));
+        await Promise.all(modifyContent.map((relativePath) => this.modifyExistingFile(relativePath)));
+
+        const filesToAdd = _.flatten([create, remove, modifyContent, rename.map(({ to }) => to)])
+        await Promise.all(filesToAdd.map((file) => git.add({ fs: fse, dir: this.path!, filepath: file })));
+
+        await git.commit({ fs: fse, dir: this.path, message, author: { name: 'Fake test author' } });
+    }
+
+    private mapToRelativePath(relativePath: string) {
+        return path.resolve(this.path!, relativePath);
+    }
+
+
+    private async createNewFile(relativePath: string) {
+        const path = this.mapToRelativePath(relativePath);
+        if (fse.existsSync(path)) {
+            throw new Error(`Attempted creating a new file at path that already exists: ${path}`);
+        }
+        await fse.createFile(path);
+        await fse.appendFile(path, `created at: ${Date.now} `);
+    }
+
+    private async deleteExistingFile(relativePath: string) {
+        const path = this.mapToRelativePath(relativePath);
+        if (!fse.existsSync(path)) {
+            throw new Error(`Attempted deleting a file that doesn't exist: ${path}`);
+        }
+        await fse.remove(path);
+    }
+
+    private async renameExistingFile(relativePath: string, newRelativePath: string) {
+        const path = this.mapToRelativePath(relativePath);
+        const newPath = this.mapToRelativePath(newRelativePath);
+        if (!fse.existsSync(path)) {
+            throw new Error(`Attempted renaming a file that doesn't exist: ${path}`);
+        }
+        await fse.rename(path, newPath);
+    }
+
+    private async modifyExistingFile(relativePath: string) {
+        const path = this.mapToRelativePath(relativePath);
+        if (!fse.existsSync(path)) {
+            throw new Error(`Attempted modifying a file that doesn't exist: ${path}`);
+        }
+        await fse.appendFile(path, `${Date.now} `);
+    }
+}
